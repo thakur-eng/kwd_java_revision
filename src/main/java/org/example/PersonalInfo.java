78a63799b9cb518bff266d2792ab96b37e56dfaf
thakur-eng <ashishthakurofficial.in@gmail.com>
2026-03-12 11:15:03 +0530

    tril version

---
+++ src/main/java/org/example/PersonalInfo.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class PersonalInfo {
+    public static void main(String[] args) {
+
+        // Printing multiple outputs using println
+        System.out.println("Name: Ganpat");
+        System.out.println("Age: 22");
+        System.out.println("City: Bangalore");
+
+    }
+}
