373ce7fd8f29130cdfcc8c92ede7726bc79d17d0
thakur-eng <ashishthakurofficial.in@gmail.com>
2026-03-12 11:15:03 +0530

    hellow

---
+++ src/main/java/org/example/HelloJava.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class HelloJava {
+    public static void main(String[] args) {
+
+        // main() is the entry point of every Java program
+        // JVM starts executing program from main method
+
+        System.out.println("Hello Java");
+
+        // System.out.println is used to print output on console
+    }
+}
